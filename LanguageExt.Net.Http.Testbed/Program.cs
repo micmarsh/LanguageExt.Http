51cab3fc16ddaa284187fe5484e046594ec5d733
Michael Marsh <mike@marsh.pw>
2025-10-22 22:48:34 -0400

    plain interfaces seem much better than CanRead

--- LanguageExt.Net.Http.Testbed/Program.cs
+++ LanguageExt.Net.Http.Testbed/Program.cs
@@ -4,25 +4,26 @@ using LanguageExt;
 using LanguageExt.Net;
 using LanguageExt.Traits;
 using static LanguageExt.Net.Http;
-
-var withInt = Readable.ask<MyApp, int>();
+using Http = LanguageExt.Http;
 
 var got =
-    from response in get<MyApp>("https://example.com")
-    from client in Readable.ask<MyApp, HttpClient>()
-    from number in Readable.ask<MyApp, int>()
-    from yo in IO.liftAsync(env => client.GetAsync($"http://example.com/{number}", env.Token))
+    from response in get<MyApp, Config>("https://example.com")
+    from config in Readable.ask<MyApp, Config>()
+    let number = config.MagicNumber
+    from yo in IO.liftAsync(env => config.Client.GetAsync($"http://example.com/{number}", env.Token))
     select yo.EnsureSuccessStatusCode();
 
-public record Config(int MagicNumber, HttpEnv Http, string MagicText);
+public record Config(int MagicNumber, HttpEnv Http, string MagicText)
+    : HasHttpClient, HasCompletionOption
+{
+    public HttpClient Client => Http.Client;
+    public Option<HttpCompletionOption> CompletionOption => Http.CompletionOption;
+}
 
 public record MyApp<A>(ReaderT<Config, IO, A> run) : K<MyApp, A>;
 
 public class MyApp: Deriving.MonadUnliftIO<MyApp, ReaderT<Config, IO>>,
-    Deriving.Readable<MyApp, Config, ReaderT<Config, IO>>,
-    CanRead<MyApp, Config, HttpEnv>,
-    CanRead<MyApp, Config, HttpClient>,
-    CanRead<MyApp, Config, int>
+    Deriving.Readable<MyApp, Config, ReaderT<Config, IO>>
 {
     public static K<ReaderT<Config, IO>, A> Transform<A>(K<MyApp, A> fa)
     {
@@ -33,42 +34,5 @@ public class MyApp: Deriving.MonadUnliftIO<MyApp, ReaderT<Config, IO>>,
     {
         throw new NotImplementedException();
     }
-
-
-    public static HttpEnv GetInner(Config env)
-    {
-        return env.Http;
-    }
-
-    static int CanRead<MyApp, Config, int>.GetInner(Config env)
-    {
-        return env.MagicNumber;
-    }
-
-    static HttpClient CanRead<MyApp, Config, HttpClient>.GetInner(Config env)
-    {
-        return env.Http.Client;
-    }
-}
-
-public interface CanRead<Self, OuterEnv, InnerEnv> : Readable<Self, InnerEnv>
-    where Self : 
-    //Functor<Self>,
-    CanRead<Self, OuterEnv, InnerEnv>,
-    Readable<Self, OuterEnv>
-{
-    public static abstract InnerEnv GetInner(OuterEnv env);
-    public static abstract OuterEnv SetInner(OuterEnv outer, InnerEnv inner);
-
-    static K<Self, InnerEnv> Readable<Self, InnerEnv>.Ask =>
-        //Readable.ask<Self, OuterEnv>().Map(Self.GetInner);
-        Self.Asks((InnerEnv x) => x);
-
-    static K<Self, A> Readable<Self, InnerEnv>.Asks<A>(Func<InnerEnv, A> f) =>
-        Readable.asks<Self, OuterEnv, A>(outer => f(Self.GetInner(outer)));
-
-    // todo need to test for this, not quite sure what's going on?
-    static K<Self, A> Readable<Self, InnerEnv>.Local<A>(Func<InnerEnv, InnerEnv> f, K<Self, A> ma) =>
-        Self.Local(outer => Self.SetInner(outer, f(Self.GetInner(outer))), ma);
 }
 
--- LanguageExt.Net.Http/Module/Http.Module.Get.cs
+++ LanguageExt.Net.Http/Module/Http.Module.Get.cs
@@ -5,26 +5,30 @@ namespace LanguageExt.Net;
 
 public partial class Http
 {
-    public static K<M, HttpResponseMessage> get<M>([StringSyntax("Uri")] string url, Option<HttpCompletionOption> option = default)
-        where M : Readable<M, HttpEnv>, MonadIO<M>
+    public static K<M, HttpResponseMessage> get<M, Env>([StringSyntax("Uri")] string url, Option<HttpCompletionOption> option = default)
+        where M : Readable<M, Env>, MonadIO<M>
+        where Env : HasHttpClient, HasCompletionOption
         =>  from uri in parseUri<IO>(url).As()
-            from httpEnv in ask<M>()
+            from httpEnv in Readable.ask<M, Env>()
             from response in getAsIO(uri, option, httpEnv)
             select response;
     
-    public static K<M, HttpResponseMessage> get<M>(Uri url, Option<HttpCompletionOption> option = default)
-        where M : Readable<M, HttpEnv>, MonadIO<M>
-        => ask<M>().Bind(httpEnv => getAsIO(url, option, httpEnv));
+    public static K<M, HttpResponseMessage> get<M, Env>(Uri url, Option<HttpCompletionOption> option = default)
+        where M : Readable<M, Env>, MonadIO<M>
+        where Env : HasHttpClient, HasCompletionOption
+        => Readable.ask<M, Env>().Bind(httpEnv => getAsIO(url, option, httpEnv));
 
     public static Http<HttpResponseMessage> get(Uri url, Option<HttpCompletionOption> option = default) =>
-        get<Http>(url, option).As();
+        get<Http, HttpEnv>(url, option).As();
     
     public static Http<HttpResponseMessage> get([StringSyntax("Uri")] string url, Option<HttpCompletionOption> option = default) =>
-        get<Http>(url, option).As();
+        get<Http, HttpEnv>(url, option).As();
     
-    private static IO<HttpResponseMessage> getAsIO(Uri url, Option<HttpCompletionOption> option, HttpEnv httpEnv) =>
-        IO.liftAsync(env => httpEnv.Client.GetAsync(url, resolveCompletion(option, httpEnv), env.Token));
+    private static IO<HttpResponseMessage> getAsIO<Env>(Uri url, Option<HttpCompletionOption> option, Env httpEnv)
+        where Env : HasHttpClient, HasCompletionOption =>
+        IO.liftAsync(env => httpEnv.Client.GetAsync(url, resolveCompletion<Env>(option, httpEnv), env.Token));
 
-    private static HttpCompletionOption resolveCompletion(Option<HttpCompletionOption> option, HttpEnv httpEnv) => 
+    private static HttpCompletionOption resolveCompletion<Env>(Option<HttpCompletionOption> option, Env httpEnv)
+        where Env : HasHttpClient, HasCompletionOption => 
         option.IfNone(httpEnv.CompletionOption.IfNone(HttpCompletionOption.ResponseContentRead));
 }
