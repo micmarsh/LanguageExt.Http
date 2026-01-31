c15fbfbb5e18a8cfb494823c04a3ea48592e8d7c
Michael Marsh <mike@marsh.pw>
2026-01-30 22:16:17 -0500

    WIP start copy-pasting over extension operators for concrete type

--- LanguageExt.Net.Http/Http.Extensions.cs
+++ LanguageExt.Net.Http/Http.Extensions.cs
@@ -15,4 +15,51 @@ public static class HttpExt
     
     public static Http<C> SelectMany<A, B, C>(this Ask<HttpEnv, A> ask, Func<A, Http<B>> bind, Func<A, B, C> project) =>
         ((Http<A>)ask).SelectMany(bind, project);
+    
+    extension<A, B>(K<Http, A> self)
+    {
+        /// <summary>
+        /// Monad bind operator
+        /// </summary>
+        /// <param name="ma">Monad to bind</param>
+        /// <param name="f">Binding function</param>
+        /// <returns>Mapped monad</returns>
+        public static Http<B> operator >> (K<Http, A> ma, Func<A, K<Http, B>> f) =>
+            +ma.Bind(f);
+        
+        /// <summary>
+        /// Sequentially compose two actions, discarding any value produced by the first, like sequencing operators (such
+        /// as the semicolon) in C#.
+        /// </summary>
+        /// <param name="lhs">First action to run</param>
+        /// <param name="rhs">Second action to run</param>
+        /// <returns>Result of the second action</returns>
+        public static Http<B> operator >> (K<Http, A> lhs, K<Http, B> rhs) =>
+            lhs >> (_ => rhs);
+    }
+    
+    extension<A>(K<Http, A> self)
+    {
+        /// <summary>
+        /// Sequentially compose two actions.  The second action is a unit-returning action, so the result of the
+        /// first action is propagated. 
+        /// </summary>
+        /// <param name="lhs">First action to run</param>
+        /// <param name="rhs">Second action to run</param>
+        /// <returns>Result of the first action</returns>
+        public static Http<A> operator >> (K<Http, A> lhs, K<Http, Unit> rhs) =>
+            lhs >> (x => (_ => x) * rhs);
+        
+        /// <summary>
+        /// Downcast operator
+        /// </summary>
+        public static Http<A> operator +(K<Http, A> ma) =>
+            (Http<A>)ma;
+        
+        /// <summary>
+        /// Downcast operator
+        /// </summary>
+        public static Http<A> operator >> (K<Http, A> ma, Lower lower) =>
+            +ma;
+    }
 }
