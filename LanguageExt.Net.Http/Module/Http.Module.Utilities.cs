a10ca9afbcf603811893f8c74b8155692ce180a0
Michael Marsh <mike@marsh.pw>
2026-01-31 09:14:48 -0500

    minimal json helpers

--- LanguageExt.Net.Http/Module/Http.Module.Utilities.cs
+++ LanguageExt.Net.Http/Module/Http.Module.Utilities.cs
@@ -1,4 +1,6 @@
 using System.Text;
+using System.Text.Json;
+using LanguageExt.Common;
 using LanguageExt.Traits;
 using static LanguageExt.Prelude;
 
@@ -8,6 +10,8 @@ public partial class Http
 {
     public static readonly Http<HttpEnv> ask = Readable.ask<Http, HttpEnv>().As();
 
+    public static Http<A> Pure<A>(A item) => new(new ReaderT<HttpEnv, IO, A>(_ => item));
+    
     public static K<M, string> readContentAsString<M>(HttpResponseMessage message)
         where M : MonadIO<M>
         => MonadIO.liftIO<M, string>(IO.liftAsync(env =>
@@ -46,4 +50,19 @@ public partial class Http
 
     public static HttpContent content(string value) =>
         new ByteArrayContent(Encoding.ASCII.GetBytes(value));
+
+    public static K<M, A> @try<M, A>(Func<A> run) where M : Applicative<M>, Fallible<M>
+        => Try.lift(run).Match(M.Pure, M.Fail<A>);
+
+    public static Http<A> @try<A>(Func<A> run) => +@try<Http, A>(run);
+
+    public static Http<JsonElement> json(HttpResponseMessage r) =>
+        readContentAsStream(r) >> (stream => @try(() => JsonDocument.Parse(stream).RootElement));
+    
+    public static Http<Result> json<Result>(HttpResponseMessage r) =>
+        from stream in readContentAsStream(r)
+        from resultNull in @try(() => JsonSerializer.Deserialize<Result>(stream))
+        from result in Optional(resultNull).Match(Pure, () => 
+            Fail<Result>(Error.New($"Could not deserialize json result")))// todo actual result as string? Maybe just abondon stream then?
+        select result;
 }
